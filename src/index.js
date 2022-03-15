b02e75ca4b29cd476e36652822a4dd0eea69496d
Fernando Maule <fer@maule.pro>
2022-03-15 20:57:22 +0100

    feat: parse TOML config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const request = require('axios');
 const fs = require('fs').promises;
+const toml = require('toml');
 
 const getMultipartForm = (script, bindings) => {
   const boundaryName = `boundary-${new Date().valueOf()}`;
@@ -33,37 +34,49 @@ const main = async () => {
   const workerName = core.getInput('workerName');
   const cfAccountId = process.env['CF_ACCOUNT_ID'];
 
-  if (!apiToken || !fileName || !workerName || !cfAccountId) {
-    throw new Error('Missing parameter');
-  }
+  // if (!apiToken || !fileName || !workerName || !cfAccountId) {
+  //   throw new Error('Missing parameter');
+  // }
 
-  const script = await fs.readFile(fileName);
-  const bindings = [
-    { name: 'SECRET', text: 'SECRET_VALUE', type: 'plain_text' },
-    {
-      name: 'KVDump',
-      type: 'kv_namespace',
-      namespace_id: '3b6f4daa5f174d9aa1fd3f0df5203a3b',
-    },
-  ];
+  const tomlFile = await fs.readFile('foo.toml');
+  const tomlData = toml.parse(tomlFile)
 
-  const cfEnvironment = 'production';
-  const url = `https://api.cloudflare.com/client/v4/accounts/${cfAccountId}/workers/services/${workerName}/environments/${cfEnvironment}`;
+  const envVars = Object.entries(tomlData.env.tst.vars)
+    .reduce((acc, [key, value]) => ({...acc, ...{ name: key, text: value, type: 'plain_text'}}), {})
+  const kvNamespaces = tomlData.env.tst.kv_namespaces.map(namespace => ({ name: namespace.binding, namespace_id: namespace.id, type: 'kv_namespace' }))
+  const workerNameFromToml = tomlData.env.tst.name
 
-  const [boundaryName, formData] = getMultipartForm(script, bindings);
-  const headers = {
-    Authorization: `Bearer ${apiToken}`,
-    'Content-Type': `multipart/form-data; boundary=----${boundaryName}`,
-  };
+  console.log(envVars)
+  console.log(kvNamespaces)
+  console.log(workerNameFromToml)
 
-  const { data } = await request.put(url, formData, {
-    headers,
-  });
+  // const script = await fs.readFile(fileName);
+  // const bindings = [
+  //   { name: 'SECRET', text: 'SECRET_VALUE', type: 'plain_text' },
+  //   {
+  //     name: 'KVDump',
+  //     type: 'kv_namespace',
+  //     namespace_id: '3b6f4daa5f174d9aa1fd3f0df5203a3b',
+  //   },
+  // ];
 
-  // const { data } = await request.put(url, file, { headers });
-  core.info(
-    `Cloudflare bundle uploaded to worker ${data.result.id} with etag ${data.result.etag} and usage model ${data.result.usage_model}`
-  );
+  // const cfEnvironment = 'production';
+  // const url = `https://api.cloudflare.com/client/v4/accounts/${cfAccountId}/workers/services/${workerName}/environments/${cfEnvironment}`;
+
+  // const [boundaryName, formData] = getMultipartForm(script, bindings);
+  // const headers = {
+  //   Authorization: `Bearer ${apiToken}`,
+  //   'Content-Type': `multipart/form-data; boundary=----${boundaryName}`,
+  // };
+
+  // const { data } = await request.put(url, formData, {
+  //   headers,
+  // });
+
+  // // const { data } = await request.put(url, file, { headers });
+  // core.info(
+  //   `Cloudflare bundle uploaded to worker ${data.result.id} with etag ${data.result.etag} and usage model ${data.result.usage_model}`
+  // );
 };
 
 main().catch((err) => {
