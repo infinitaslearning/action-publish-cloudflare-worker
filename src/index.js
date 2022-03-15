1ce89faeafeb305f9cb41af366c1af2dd74256d7
Fernando Maule <fer@maule.pro>
2022-03-15 20:17:07 +0100

    feat: use multipart form to publish script and metadata

--- src/index.js
+++ src/index.js
@@ -2,25 +2,74 @@ const core = require('@actions/core');
 const request = require('axios');
 const fs = require('fs').promises;
 
+const getMultipartForm = (script, bindings) => {
+  const boundaryName = `boundary-${new Date().valueOf()}`;
+  const boundaryString = `------${boundaryName}`;
+  const multipartEOF = `${boundaryString}--`;
+
+  const scriptPart = [
+    boundaryString,
+    'Content-Disposition: form-data; name="script"; filename="blob"',
+    'Content-Type: application/javascript\n',
+    script,
+  ].join('\n');
+
+  const metadataPart = [
+    boundaryString,
+    'Content-Disposition: form-data; name="metadata"; filename="blob"',
+    'Content-Type: application/json\n',
+    JSON.stringify({
+      bindings,
+      body_part: 'script',
+    }),
+  ].join('\n');
+
+  return [boundaryName, `${scriptPart}\n${metadataPart}\n${multipartEOF}`];
+};
+
 const main = async () => {
-    const apiToken = core.getInput('apiToken');
-    const fileName = core.getInput('fileName');
-    const workerName = core.getInput('workerName');
-    const cfAccountId = process.env['CF_ACCOUNT_ID'];
+  const apiToken = core.getInput('apiToken');
+  const fileName = core.getInput('fileName');
+  const workerName = core.getInput('workerName');
+  const cfAccountId = process.env['CF_ACCOUNT_ID'];
 
-    if(!apiToken || !fileName || !workerName || !cfAccountId){
-        throw new Error('Missing parameter');
-    }
+  if (!apiToken || !fileName || !workerName || !cfAccountId) {
+    throw new Error('Missing parameter');
+  }
 
-    const url = `https://api.cloudflare.com/client/v4/accounts/${cfAccountId}/workers/scripts/${workerName}`;
-    const headers = {
-        'Authorization': `Bearer ${apiToken}`,
-        'Content-Type': 'application/javascript'
-    };
+  const script = await fs.readFile(fileName);
+  const bindings = [
+    { name: 'SECRET', text: 'SECRET_VALUE', type: 'plain_text' },
+    {
+      name: 'KVDump',
+      type: 'kv_namespace',
+      namespace_id: '3b6f4daa5f174d9aa1fd3f0df5203a3b',
+    },
+  ];
 
-    const file = await fs.readFile(fileName);
-    const { data } = await request.put(url, file, { headers });
-    core.info(`Cloudflare bundle uploaded to worker ${data.result.id} with etag ${data.result.etag} and usage model ${data.result.usage_model}`);
-}
+  const cfEnvironment = 'production';
+  const url = `https://api.cloudflare.com/client/v4/accounts/${cfAccountId}/workers/services/${workerName}/environments/${cfEnvironment}`;
 
-main().catch(err => core.setFailed(err.message));
+  const [boundaryName, formData] = getMultipartForm(script, bindings);
+  const headers = {
+    Authorization: `Bearer ${apiToken}`,
+    'Content-Type': `multipart/form-data; boundary=----${boundaryName}`,
+  };
+
+  const { data } = await request.put(url, formData, {
+    headers,
+  });
+
+  // const { data } = await request.put(url, file, { headers });
+  core.info(
+    `Cloudflare bundle uploaded to worker ${data.result.id} with etag ${data.result.etag} and usage model ${data.result.usage_model}`
+  );
+};
+
+main().catch((err) => {
+  if (err.response?.data) {
+    core.error(err.response.data.errors);
+  }
+  console.error(err);
+  core.setFailed(err.message);
+});
